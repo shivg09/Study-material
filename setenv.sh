62d2d6995b51222c190c31a5ea1a8987b968a027
shivg09 <47523651+shivg09@users.noreply.github.com>
2022-12-21 10:42:48 +0530

    Create setenv.sh

---
+++ setenv.sh
@@ -0,0 +1,4 @@
+seroadmin@prod-LIVE:/opt/serosoft/tomcat-live/bin$ cat setenv.sh
+export JAVA_OPTS="$JAVA_OPTS  -Xmx25g -Xms25g -Xverify:none -Djava.awt.headless=true -Dfile.encoding=UTF-8 -XX:+UseCompressedOops -XX:+UseConcMarkSweepGC -XX:+UseParNewGC -XX:+CMSClassUnloadingEnabled -XX:+CMSParallelRemarkEnabled -XX:+CMSCompactWhenClearAllSoftRefs -XX:CMSInitiatingOccupancyFraction=85 -XX:+CMSScavengeBeforeRemark -XX:+CMSConcurrentMTEnabled -XX:ParallelCMSThreads=8 -XX:+DisableExplicitGC -XX:MaxHeapFreeRatio=70 -XX:MinHeapFreeRatio=40 -XX:MaxTenuringThreshold=2 -XX:NewSize=8g -XX:MaxNewSize=8g -XX:SurvivorRatio=4 -XX:TargetSurvivorRatio=85 -XX:InitialCodeCacheSize=256m -XX:ReservedCodeCacheSize=512m -XX:ParallelGCThreads=8 -XX:ConcGCThreads=3"
+export JAVA_HOME='/usr/lib/jvm/java-8-openjdk-amd64/jre'
+JAVA_OPTS="$JAVA_OPTS -javaagent:/opt/newrelic/newrelic.jar"
