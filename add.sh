684c6a4e389d0c2f41195d143f90c6c1aa949c37
shivg09 <47523651+shivg09@users.noreply.github.com>
2023-04-09 10:30:41 +0530

    Update add.sh

--- add.sh
+++ add.sh
@@ -7,3 +7,16 @@ read second
 echo `expr $first + $second`
 
 echo "$sum"
+===========================
+
+#!/bin/bash
+
+echo "enter the value of a:"
+read a
+echo "enter the value of b:"
+read b
+
+sum=$((`expr $a + $b `))
+echo "=========================="
+echo "Sum value=$sum"
+~
